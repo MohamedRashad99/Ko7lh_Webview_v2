ea3e8cb2fa04be4edcc289f00f27e53ec1119d8f
Mohamed Rashad <Mohamedrashad9797@gmail.com>
2021-08-26 18:05:51 +0200

    update not completed

---
+++ lib/src/elements/RaisedGradientButton.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RaisedGradientButton extends StatelessWidget {
+  final Widget child;
+  final Gradient gradient;
+  final double width;
+  final double height;
+  final VoidCallback? onPressed;
+
+  const RaisedGradientButton({
+
+    required this.child,
+    required this.gradient,
+    this.width = double.infinity,
+    this.height = 50.0,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 50.0,
+      decoration: BoxDecoration(
+          gradient: gradient,
+          borderRadius: BorderRadius.all(Radius.circular(80.0)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey[500]!,
+              offset: Offset(0.0, 1.5),
+              blurRadius: 1.5,
+            ),
+          ]),
+      child: Material(
+        color: Colors.transparent,
+        child: InkWell(
+            onTap: onPressed!,
+            child: Center(
+              child: child,
+            )),
+      ),
+    );
+  }
+}
